223356417f809aa24a6e6f72e8d8bd61a5cb8442
Doemin04 <minh.npn@gmail.com>
2023-10-06 20:00:29 -0400

    Add basic GUI components

--- Weather_App/WeatherApp.java
+++ Weather_App/WeatherApp.java
@@ -14,25 +14,48 @@ public class WeatherApp {
     private WeatherPanel weatherPanel;
 
     public WeatherApp() {
+        frame = new JFrame("Weather App");
+        frame.setSize(400, 300);
+        frame.setLayout(new BorderLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        textArea = new JTextArea();
+        textArea.setEditable(false);
+        textArea.setWrapStyleWord(true);
+        textArea.setLineWrap(true);
+
+        fetchButton = new JButton("Fetch Weather");
+        
+
+        weatherPanel = new WeatherPanel();
+        
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        
+        frame.add(scrollPane, BorderLayout.CENTER);
+        frame.add(textArea, BorderLayout.CENTER);
+        frame.add(fetchButton, BorderLayout.SOUTH);
+        frame.add(weatherPanel, BorderLayout.NORTH);
+        frame.setVisible(true);
     }
 
     private String fetchDataFromAPI() {
         try {
-
+            String result = "yes";
+                return result;
         } catch(Exception e){
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-    SwingUtilities.invokeLater(new Runnable() {
-        @Override
-        public void run() {
-            //new WeatherApp();
-        }
-    });
-}
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new WeatherApp();
+            }
+        });
+    }
 }
 
 class WeatherPanel extends JPanel {
