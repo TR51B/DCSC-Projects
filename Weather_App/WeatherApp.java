13b1380dfa0cc21943057ecdf66ba89ca05e3130
Tisha1503 <130492564+Tisha1503@users.noreply.github.com>
2023-10-17 21:36:54 -0400

    Update WeatherApp.java

--- Weather_App/WeatherApp.java
+++ Weather_App/WeatherApp.java
@@ -21,12 +21,27 @@ public class WeatherApp {
         frame.setSize(420,420); // sets the x-dimention, and y-dimention of frame
         frame.setVisible(true); //make frame visible
     }
+    private String FetchAPI(location){
+        private String API_KEY = "YOUR_API_KEY";
+        private String location = "London"
+        private String APIurl = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}";
+        URL url = new URL(APIurl  + cityName + "&appid=" + API_KEY);
+        try{
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader rdr = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            StringBuilder res = new StringBuilder();
+        }
 
-    private String fetchDataFromAPI() {
+    }
+
+    private String fetchDataFromAPI(location) {
         try {
 
         } catch(Exception e){
             e.printStackTrace();
+            return "Exception error!"
         }
     }
 
